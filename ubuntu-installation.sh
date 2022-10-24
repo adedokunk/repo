5dba416f206e2bef79d53de0bb68f18739ae4389
kazeem <aniyalorungade@gmail.com>
2022-10-23 20:56:32 -0400

    change

---
+++ ubuntu-installation.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+  # Author Kazeem----
+  # Date---- Oct-23-2023
+  #description--  script that install pkg on ubuntu server---
+
+apt  wget
+apt net-utils
+apt sysstat
+apt finger
+apt gcc
+apt  make
+apt python3
+apt  epel-release
+apt  git
+
+echo "packages installed successful"
